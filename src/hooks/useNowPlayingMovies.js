eb2a182992ad6f7828595033d48c40a510603f1e
barthwalneha <nbarthwal30@gmail.com>
2024-11-02 00:26:52 +0530

    getting data from TMDB & custome hook for now playin movies

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+
+//custom hook which is fetching data from TMdb and updating store
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
+  };
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
+export default useNowPlayingMovies;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzQxNjk1MTQ5M2JjZjE4MTBmYmEwMDBkZWYwNWU0NyIsIm5iZiI6MTczMDQ1MTUwNi43NzkxNzksInN1YiI6IjY3MjQ5NjUyYzEyMTIzMTNhYzdlYzFhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l6we1Ej3NbJc5eoacUVkHiR7I-A4uravPcjpr0sD6uk",
+  },
+};
